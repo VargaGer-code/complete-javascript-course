408c87570b25856335029fca2f242ae1e6e4a22b
vGerAsh <gergely.varga@ashszoftverhaz.hu>
2023-05-12 17:50:40 +0200

    finished pig game

--- 07-Pig-Game/finished/script.js
+++ 07-Pig-Game/finished/script.js
@@ -1,28 +1,99 @@
-"use strict";
+'use strict';
 
-// Selecting elemets
-const diceEL = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn-new");
-const btnRoll = document.querySelector(".btn-roll");
-const btnHold = document.querySelector(".btn-hold");
-const score0EL = document.querySelector("#score--00");
-const score1EL = document.querySelector("#score--01");
-const player0EL = document.querySelector(".player--0");
-const player1EL = document.querySelector(".player--1");
+// Selecting player elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
-// Startin conditions
-score0EL.textContent = 0;
-score1EL.textContent = 0;
-diceEL.classList.add("hidden");
+// Selecting dice, btn elements
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Declare variables and than assign values to them in the init function
+let scores, currentScore, activePlayer, playing, pointsToWin;
+
+// Starting conditions
+const init = function () {
+  pointsToWin = 100;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--winner');
+};
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // Switch backgrounds
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+init();
 
 // Rolling the dice
-btnRoll.addEventListener("click", function () {
-  // Generate random number
-  const dice = Math.trunc(Math.random() * 6) + 1;
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generate a random number
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // display dice
-  diceEL.classList.remove("hidden");
-  diceEL.src = `dice-${dice}.png`;
-
-  // Check for rolled 1: if true, switch player
+    // 2. Check if rolle 1: if true, switch player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch player
+      switchPlayer();
+    }
+  }
 });
+
+// Clicking on btn hold
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active players score
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check score if >= winningScore
+    if (scores[activePlayer] >= pointsToWin) {
+      // Finish game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // 3. Switch players
+      switchPlayer();
+    }
+  }
+});
+
+// Starting newGame
+btnNew.addEventListener('click', init);
