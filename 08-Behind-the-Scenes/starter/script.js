759e1b87061f781fc41f6afbf145b2d023b2b194
vGerAsh <gergely.varga@ashszoftverhaz.hu>
2023-05-17 16:22:30 +0200

    Data structures learn from office

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const addExpr = function () {
+  console.log(arguments);
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return console.log(`SUM :: ${sum}`);
+};
+
+addExpr(2, 5, 7, 3, 1);
+
+let addArrow = (a, b) => {
+  console.log(arguments);
+};
+addArrow();
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,45 @@ const restaurant = {
     },
   },
 };
+
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+const arr2 = [3, 4, 5];
+const arr3 = ['a', 'b', 'c'];
+const newArr = [1, 2, ...arr2];
+
+console.log(newArr);
+console.log(...newArr);
+
+let mergedArrays = [...arr2, ...arr3];
+console.log('mergedArrays :: ' + mergedArrays);
+
+const str = 'epic text';
+const spreadStr = [...str];
+console.log(spreadStr);
+
+const arrayFood = ['apple', 'pear', 'orange'];
+
+const writeIt = function (arg1, arg2, arg3) {
+  console.log(`Here are the fruits: ${arg1}, ${arg2}, ${arg3}`);
+};
+
+writeIt(...arrayFood);
+
+const [a, , , b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// REST pattern
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(1, 2, 4);
+add(1, 2, 4, 5, 6);
+
+restaurant.orer && restaurant.order('cheese', 'tomato');
+
+console.log(0 ?? 10);
